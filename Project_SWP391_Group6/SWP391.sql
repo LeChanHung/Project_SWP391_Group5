151b84a04a7e82e0368e44a2663a6b2c2188e84b
LeChanHung <hunglche160179@fpt.edu.vn>
2024-01-23 21:35:06 +0700

    update Database

--- Project_SWP391_Group6/SWP391.sql
+++ Project_SWP391_Group6/SWP391.sql
@@ -1,11 +1,7 @@
-create table class (
-	class_ID int primary key,
-	class_name nvarchar(100),
-	"date" date,
-
-);
 create table account (
 	role_ID int primary key,
+	email nvarchar(50),
+	password nvarchar(50),
 	role int,
 );
 
@@ -15,12 +11,13 @@ create table "admin" (
 	adPassword nvarchar(50),
 	role_ID int,
 	foreign key (role_ID) references account(role_ID),
-
+	role int,
 );
 
 create table student (
 	student_ID int primary key,
 	student_name nvarchar(50),
+	student_age int,
 	student_email nvarchar(50),
 	student_password nvarchar(50),
 	student_msv varchar(8),
@@ -28,10 +25,8 @@ create table student (
 	role_ID int,
 	dob date,
 	gender varchar(10),
-	class_ID int,
 	foreign key (role_ID) references account(role_ID),
-	foreign key (class_ID) references class(class_ID),
-
+	role int,
 );
 
 create table teacher (
@@ -41,20 +36,9 @@ create table teacher (
 	teacher_password nvarchar(50),
 	role_ID int,
 	dob date,
-	class_ID int,
 	gender varchar(10),
 	foreign key (role_ID) references account(role_ID),
-	foreign key (class_ID) references class(class_ID),
-
-);
-
-create table class_teacher_mapping(
-	teacher_ID int,
-	class_ID int,
-	primary key(teacher_ID,class_ID),
-	foreign key (teacher_ID) references teacher(teacher_ID),
-	foreign key (class_ID) references class(class_ID),
-	teacher_name varchar(200),
+	role int,
 );
 
 create table "phong_dao_tao" (
@@ -63,7 +47,7 @@ create table "phong_dao_tao" (
 	systemUser_password nvarchar(50),
 	role_ID int,
 	foreign key (role_ID) references account(role_ID),
-
+	role int,
 );
 
 create table feedback (
@@ -78,26 +62,57 @@ create table "subject" (
 	subject_name nvarchar(100),
 
 );
-create table lesson (
-	lesson_ID int primary key,
-	lesson_name nvarchar(250),
-	subject_ID int,
-	foreign key (subject_ID) references "subject"(subject_ID),
-	"date" date,
-);
 
+create table class (
+	class_ID int primary key,
+	student_ID int,
+	subject_ID int,
+	teacher_ID int,
+	foreign key (student_ID) references student(student_ID),
+	foreign key (subject_ID) references "subject"(subject_ID),
+	foreign key (teacher_ID) references teacher(teacher_ID),
+	class_name nvarchar(100),
+	"date" date,
+
+);
 
 create table attendance (
 	attendance_ID int primary key,
-	lesson_ID int,
-	foreign key (lesson_ID) references lesson(lesson_ID),
 	student_ID int,
-	foreign key (student_ID) references student(student_ID),
+	subject_ID int,
 	teacher_ID int,
+	foreign key (student_ID) references student(student_ID),
+	foreign key (subject_ID) references "subject"(subject_ID),
 	foreign key (teacher_ID) references teacher(teacher_ID),
 	"status" bit,
 	"date" date,
 
 );
 
+create table lesson (
+	lesson_ID int primary key,
+	lesson_name nvarchar(250),
+	subject_ID int,
+	foreign key (subject_ID) references "subject"(subject_ID),
+	student_ID int,
+	foreign key (student_ID) references student(student_ID),
+	teacher_ID int,
+	foreign key (teacher_ID) references teacher(teacher_ID),
+	attendance_ID int,
+	foreign key (attendance_ID) references attendance(attendance_ID),
+	"date" date,
+);
+
+create table class_subject_mapping (
+	subject_ID int,
+	class_ID int,
+	lesson_ID int,
+	primary key(subject_ID, class_ID,lesson_ID),
+	foreign key (subject_ID) references "subject"(subject_ID),
+	foreign key (class_ID) references class(class_ID),
+	foreign key (lesson_ID) references lesson(lesson_ID),
+	class_name nvarchar(100),
+	subject_name nvarchar(100),
+	"date" date,
+);
 
