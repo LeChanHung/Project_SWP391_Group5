6306ec986316cba4e12f98ad640669959a2f3d06
LeChanHung <hunglche160179@fpt.edu.vn>
2024-02-27 23:54:01 +0700

    update function forget password

--- Project_SWP391_Group6/Test_Function_Project/src/java/Controller/Profile.java
+++ Project_SWP391_Group6/Test_Function_Project/src/java/Controller/Profile.java
@@ -13,9 +13,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 
 /**
